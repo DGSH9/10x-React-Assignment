7707192ffa0513013aa837169ac9dae9f39d5f6b
Durgesh Kumar Yadav <57389386+DGSH9@users.noreply.github.com>
2021-12-16 15:17:19 +0530

    Fixing margin between text content of employee class

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -1,32 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
 
+function App(props) {
+  const {employee}=props;
 
-
-function App() {
-  const employee = {
-    name: 'Tom',
-    location: 'New York',
-    bloodGroup: 'B+',
-    age: 28,
-    profileImg: 'https://www.w3schools.com/howto/img_avatar.png'
-         }
-    
-return(
+ return(
   <div className="container">
-      <div>
-    <img src={employee.profileImg} />
-    <h1>{employee.name}</h1>
-    <p>Location</p>
-    <h4>{employee.location}</h4>
-    <p>Blood group</p>
-    <h4>{employee.bloodGroup}</h4>
-    <p>Age</p>
-    <h4>{employee.age}</h4>
 
-      </div>
+    <div className="profile-img-container">
+    <img src={employee.profileImg} width="100" height="100" alt="profile-avatar"></img>
+    </div>
+
+    <div className="title-container">
+      <h1>{employee.name}</h1>
+    </div>
+
+  <div>
+      <div className="profile-info-container">
+                <label>
+                    Location
+                </label>
+                <strong>
+                  {employee.location}
+                </strong>
+          
+        </div>
+
+      <div className="profile-info-container">
+                <label>
+                    BLood Group
+                </label>
+                <strong>
+                  {employee.bloodGroup}
+                </strong>
+        </div>
+
+      <div className="profile-info-container">
+                <label>
+                  Age
+                </label>
+                <strong>
+                  {employee.age}
+                </strong>
+
+        </div>
+
+  </div>
 
 </div>
 );
 }
+
+
 export default App;
